3b1a403864ec4b685cd94eb3aebd360f26bc46f6
Jacob-Boardman <45844667+Jacob-Boardman@users.noreply.github.com>
2020-12-14 16:27:55 +0000

    Updated for origin master pull

--- src/main/java/com/mealplanner/entity/GetRecipe.java
+++ src/main/java/com/mealplanner/entity/GetRecipe.java
@@ -1,13 +1,14 @@
 package com.mealplanner.entity;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class GetRecipe {
     private int id;
 
     private String recipe_name;
 
-    private List<IngredientEntity> ingredientList = new ArrayList<Object>();
+    private List<IngredientEntity> ingredientList = new ArrayList<>();
 
     public String getRecipe_name() {
         return recipe_name;
