dbd9476591b27472b3f03140c965a80f8c029267
Malverick <jamesfirth95@yahoo.co.uk>
2020-12-15 14:33:25 +0000

    Added Main method back in

---
+++ src/main/java/com/mealplanner/BackEnd.java
@@ -0,0 +1,16 @@
+package com.mealplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+@SpringBootApplication
+public class BackEnd {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEnd.class, args);
+    }
+
+}
