197f5cbb48b89f866edc68d28bbf35804c38b0fa
Jacob-Boardman <45844667+Jacob-Boardman@users.noreply.github.com>
2020-12-15 14:06:23 +0000

    Updated code flow for link table

--- src/main/java/com/mealplanner/controller/RecipeController.java
+++ src/main/java/com/mealplanner/controller/RecipeController.java
@@ -3,6 +3,6 @@ package com.mealplanner.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class BaseController {
+public class RecipeController {
     
 }
