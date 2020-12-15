d27d76685c4e65c1895d2a8dc44faf6f2705e256
Malverick <jamesfirth95@yahoo.co.uk>
2020-12-15 14:54:29 +0000

    Recipes controller added

--- src/main/java/com/mealplanner/controller/RecipeController.java
+++ src/main/java/com/mealplanner/controller/RecipeController.java
@@ -1,8 +1,43 @@
 package com.mealplanner.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.mealplanner.entity.RecipeEntity;
+import com.mealplanner.service.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping(path = "/recipe")
 public class RecipeController {
-    
+
+    private RecipeService recipeService;
+
+    private RecipeController (@Autowired RecipeService recipeService){
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping(path = "/get/All")
+    public ResponseEntity<List<RecipeEntity>> getrecipes(){
+        return new ResponseEntity<>(recipeService.getAllrecipes(), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/get/{id}")
+    public ResponseEntity<Optional<RecipeEntity>> getrecipe(@PathVariable("id") Integer id){
+        Optional<RecipeEntity> existingRecipe = this.recipeService.getrecipe(id);
+        if (existingRecipe.isPresent()){
+            return ResponseEntity.ok(existingRecipe);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping(path = "/add")
+    public ResponseEntity<RecipeEntity> addrecipe(@RequestBody RecipeEntity recipeEntity){
+        RecipeEntity newRecipe = recipeService.addRecipe(recipeEntity);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newRecipe);
+    }
 }
