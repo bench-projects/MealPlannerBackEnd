6d0dd72e3d4b24a2080552458e30b7fa3e0c9e4e
Malverick <jamesfirth95@yahoo.co.uk>
2020-12-16 13:33:52 +0000

    Unit tests for PUT and POST ingredient Done

--- src/test/java/com/mealplanner/unitTests/IngredientsUnitTests.java
+++ src/test/java/com/mealplanner/unitTests/IngredientsUnitTests.java
@@ -13,6 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -27,9 +28,10 @@ public class IngredientsUnitTests {
     IngredientsService ingredientsService;
 
     @Test
-    public void getAllIngredientsTest(){
+    public void getAllIngredientsTest() {
         IngredientEntity ingredientEntity = new IngredientEntity();
         List<IngredientEntity> ingredientEntities = new ArrayList<IngredientEntity>();
+        ingredientEntities.add(ingredientEntity);
 
         Mockito.when(this.ingredientsService.getAllIngredients()).thenReturn(ingredientEntities);
 
@@ -37,6 +39,34 @@ public class IngredientsUnitTests {
         assertThat(this.ingredientsController.getAllIngredients().getStatusCodeValue()).isEqualTo((200));
     }
 
-    
+    @Test
+    public void getIngredientTest() {
+        IngredientEntity ingredientEntity = new IngredientEntity("test", 5);
+
+        Mockito.when(this.ingredientsService.getIngredient(0)).thenReturn(java.util.Optional.of(ingredientEntity));
+
+        assertThat(this.ingredientsController.getIngredient(0).getBody()).isEqualTo((Optional.of(ingredientEntity)));
+        assertThat(this.ingredientsController.getIngredient(0).getStatusCodeValue()).isEqualTo((200));
+    }
+
+    @Test
+    public void addIngredientTest() {
+        IngredientEntity ingredientEntity = new IngredientEntity("test", 5);
+
+        Mockito.when(this.ingredientsService.addIngredient(ingredientEntity)).thenReturn(ingredientEntity);
+
+        assertThat(this.ingredientsController.addIngredient(ingredientEntity).getBody()).isEqualTo((ingredientEntity));
+        assertThat(this.ingredientsController.addIngredient(ingredientEntity).getStatusCodeValue()).isEqualTo((201));
+    }
+
+    @Test
+    public void updateIngredientEntityTest() {
+        IngredientEntity ingredientEntity = new IngredientEntity();
+
+        Mockito.when(this.ingredientsService.updateIngredientEntity(0, ingredientEntity)).thenReturn(Optional.of(ingredientEntity));
+
+        assertThat(this.ingredientsController.updateIngredientEntity(0, ingredientEntity).getBody()).isEqualTo((Optional.of(ingredientEntity)));
+        assertThat(this.ingredientsController.updateIngredientEntity(0, ingredientEntity).getStatusCodeValue()).isEqualTo((200));
+    }
 
 }
