78b3746b68dec07456661f02f3f25bdc2b8eb85f
Jacob-Boardman <45844667+Jacob-Boardman@users.noreply.github.com>
2020-12-23 17:29:22 +0000

    Updateing ingredietns to work with many to many

--- src/main/java/com/mealplanner/repository/IngredientsRepository.java
+++ src/main/java/com/mealplanner/repository/IngredientsRepository.java
@@ -3,6 +3,28 @@ package com.mealplanner.repository;
 import com.mealplanner.entity.IngredientEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IngredientsRepository extends JpaRepository<IngredientEntity, Integer> {
+import javax.persistence.EntityManager;
+import java.util.Optional;
+
+public abstract class IngredientsRepository implements JpaRepository<IngredientEntity, Integer> {
+
+    private EntityManager entityManager;
+
+    public IngredientsRepository(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public Optional<IngredientEntity> save(IngredientEntity ingredientEntity){
+        try{
+            entityManager.getTransaction().begin();
+            entityManager.persist(ingredientEntity);
+            entityManager.getTransaction().commit();
+            return Optional.of(ingredientEntity);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return Optional.empty();
+    }
 
 }
